b33e575962af879ddfd5c2dae7b6b638d1c946bb
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2020-12-02 18:43:12 +0100

    Non-vector image option

--- saveFigure.m
+++ saveFigure.m
@@ -1,11 +1,16 @@
-function saveFigure(figSave,absBaseFilePath)
+function saveFigure(figSave,absBaseFilePath,vectorFlag)
+if ~exist('vectorFlag','var')
+    vectorFlag = 1;
+end
 absBaseFilePath = string(absBaseFilePath); fileExt = [".fig";".pdf";".emf"];
 eidff = arrayfun(@(x) ~exist(absBaseFilePath + x,'file'),...
     fileExt);
 if eidff(1)
     savefig(figSave, absBaseFilePath + fileExt(1))
 end
-figSave = configureFigureToPDF(figSave);
+if vectorFlag
+    figSave = configureFigureToPDF(figSave);
+end
 if eidff(2)
     print(figSave, absBaseFilePath + fileExt(2),'-dpdf','-fillpage')
 end
