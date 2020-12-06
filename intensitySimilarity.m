a7d6cf781ba33b7c12f651cd38a5d9b9ebb82eba
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2020-12-06 20:43:36 +0100

    Functionality for the similarity

--- intensitySimilarity.m
+++ intensitySimilarity.m
@@ -1,21 +1,29 @@
-[Nr, Nc] = size(A);
-nr = 6.5;
-ns = ceil(nr);
+function [simMask] = intensitySimilarity(img, radius)
+if ~strcmpi(class(img),'double')
+    img = double(img);
+end
+[Nr, Nc] = size(img);
+if ~exist('radius', 'var')
+    radius = 6.5;
+end
+ns = ceil(radius);
 nrect = 2*ns + 1;
-intSim = @(cp, np, im) exp(-((im(cp(2),cp(1)) - im(np).^2) ./ var(im(np))));
-cimg = double(A);
-intSimImg = zeros(size(A));
+intSim = @(cp, np, im) exp(-((im(cp(2),cp(1)) - im(np).^2)) /...
+    (2*std(im(np))^2));
+simMask = zeros(size(img),'gpuArray');
 for cc = 1:Nc
     x = repmat((cc-ns):(cc+ns), nrect, 1);
     for cr = 1:Nr
         y = repmat(((cr-ns):(cr+ns))', 1, nrect);
         % circle equation & inside the image (& not centre?)
         coords = cat(3, x, y); C = cat(3, cc, cr);
-        relIdx = sqrt(sum((coords - C).^2,3)) <= nr & all(coords >= 1,3)...
-            & all(coords <= cat(3, Nc, Nr), 3) & any(coords ~= C, 3); 
-        ncp = sum(relIdx(:)); cpSubs = coords(repmat(relIdx(:),2,1)); 
+        relIdx = sqrt(sum((coords - C).^2,3)) <= radius & all(coords >= 1,3)...
+            & all(coords <= cat(3, Nc, Nr), 3) & any(coords ~= C, 3);
+        ncp = sum(relIdx(:)); cpSubs = coords(repmat(relIdx(:),2,1));
         cpLinSubs = sub2ind([Nr,Nc],cpSubs((ncp+1):(ncp*2)),cpSubs(1:ncp));
-        intSimImg(cr,cc) = mean(intSim([cc,cr], cpLinSubs, cimg),'omitnan');
+        simMask(cr,cc) = mean(intSim([cc,cr], cpLinSubs, img),'omitnan');
     end
 end
-figure; imagesc(intSimImg); axis image; axis off;
+figure; imagesc(simMask); axis image; axis off;
+
+end
