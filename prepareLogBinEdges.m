56f5721243a895a700393a072f830f77a865da7b
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2021-02-05 12:21:08 +0100

    Bug fix

--- prepareLogBinEdges.m
+++ prepareLogBinEdges.m
@@ -1,6 +1,7 @@
 function [binCenters, binEdges, lData, ts] = prepareLogBinEdges(data, nBins)
-lData = log10(data); mnVal = min(lData); mxVal = max(lData);
-dRange = range(lData); ts = dRange / (nBins - 1);
+lData = log10(data); lData(isinf(lData)) = []; 
+mxVal = max(lData, [], 'omitnan'); mnVal = min(lData,[],'omitnan'); 
+dRange = mxVal - mnVal; ts = dRange / (nBins - 1);
 binCenters = mnVal:ts:mxVal; binDomain = [mnVal;mxVal] + [-1;1]*(ts/2);
 binEdges = binDomain(1):ts:binDomain(2);
 end
