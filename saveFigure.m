76a945d623d37b08285db2ac36e73a3da3c34a12
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2021-05-12 15:18:37 +0200

    Added an overwriting flag

--- saveFigure.m
+++ saveFigure.m
@@ -1,19 +1,23 @@
-function saveFigure(figSave,absBaseFilePath,vectorFlag)
+function saveFigure(figSave,absBaseFilePath, vectorFlag, ovrWriteFlag)
 if ~exist('vectorFlag','var')
     vectorFlag = 1;
 end
+if ~exist('ovrWriteFlag','var')
+    ovrWriteFlag = false;
+end
+
 absBaseFilePath = string(absBaseFilePath); fileExt = [".fig";".pdf";".emf"];
 eidff = arrayfun(@(x) ~exist(absBaseFilePath + x,'file'),...
     fileExt);
-if eidff(1)
+if eidff(1) || ovrWriteFlag
     savefig(figSave, absBaseFilePath + fileExt(1))
 end
 if vectorFlag
     figSave = configureFigureToPDF(figSave);
 end
-if eidff(2)
+if eidff(2) || ovrWriteFlag
     print(figSave, absBaseFilePath + fileExt(2),'-dpdf','-fillpage')
 end
-if eidff(3)
+if eidff(3) || ovrWriteFlag
     print(figSave, absBaseFilePath + fileExt(3),'-dmeta')
 end
