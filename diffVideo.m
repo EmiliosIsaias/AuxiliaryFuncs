7bdebdf0e87a94c99c6fe5da81a0f6e61581683e
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2021-08-24 21:11:33 +0200

    Trying out batches of video

--- diffVideo.m
+++ diffVideo.m
@@ -1,21 +1,8 @@
 function diffSignal = diffVideo(videoPath)
 vidObj = VideoReader(videoPath);
-if vidObj.hasFrame
-    frame = getFrame(vidObj);
-else
-    fprintf(2, "The given path contains no frames. Maybe it wasn't a video/n")
-    return
-end
-diffSignal = zeros(vidObj.NumFrames - 1,1); vidCount = 1;
-while vidObj.hasFrame
-    nextFrame = getFrame(vidObj);
-    frameDiff = diff(cat(3, frame, nextFrame), 1, 3);
-    % Log10 of the squared sum from all pixels
-    diffSignal(vidCount) = log10(sum(frameDiff(:).^2));
-    frame = nextFrame; vidCount = vidCount + 1;
-end
-end
+mem = memory;
+maxMem = mem.MaxPossibleArrayBytes;
+frameByte = vidObj.Height * vidObj.Width * 3;
+framesInMem = round((maxMem/frameByte) * 0.85);
 
-function frame = getFrame(vidObj)
-frame = vidObj.readFrame; frame = mean(frame, 3, 'omitnan');
 end
