d69defd45d94722fc2efbeade44855c2194c1985
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2021-09-01 08:55:33 +0200

    Implemented RAM improvements

--- diffVideo.m
+++ diffVideo.m
@@ -1,8 +1,30 @@
-function diffSignal = diffVideo(videoPath)
-vidObj = VideoReader(videoPath);
-mem = memory;
-maxMem = mem.MaxPossibleArrayBytes;
-frameByte = vidObj.Height * vidObj.Width * 3;
-framesInMem = round((maxMem/frameByte) * 0.85);
-
+function smudMeanPx = diffVideo(videoPath)
+vidObj = VideoReader(videoPath); Nf = 0; fr = 0;
+smudMeanPx = [];
+if vidObj.hasFrame
+    fprintf(1, 'Calculating total number of frames...\n');
+    Nf = vidObj.NumFrames; fr = vidObj.FrameRate;
+    smudMeanPx = zeros(Nf, 1, 'single');
+else
+    fprintf(2, "The given video has no frames!/n")
+    return
+end
+frCount = 0; auxFrame = [];
+while vidObj.hasFrame && frCount < Nf
+    mem = memory; maxMem = mem.MemAvailableAllArrays;
+    frameByte = vidObj.Height * vidObj.Width * 3;
+    possFramesInMem = round((maxMem/frameByte) * 0.73);
+    if frCount + possFramesInMem > Nf
+        possFramesInMem = Nf - frCount;
+    end
+    fprintf(1, 'Reading %d/%d frames...\n', frCount + possFramesInMem, Nf)
+    frames = vidObj.read(frCount + [1, possFramesInMem]);
+    frames(:,:,[2,3],:) = [];
+    frames = squeeze(frames);
+    frames = cat(3, auxFrame, frames);
+    deltaFrame = diff(frames, 1, 3); meanPx = squeeze(mean(deltaFrame, [1, 2]));
+    smudMeanPx(1+frCount: frCount+length(meanPx)) = movmedian(meanPx, 65);
+    % Auxiliary variables
+    auxFrame = frames(:,:,end);
+    frCount = frCount + possFramesInMem;
 end
