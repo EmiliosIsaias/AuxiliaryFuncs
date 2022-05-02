50b446601db5dfa2f0da71a6bf0a32db38b3ee06
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-05-02 13:33:42 +0200

    Update transferScript.m

--- transferScript.m
+++ transferScript.m
@@ -2,9 +2,9 @@ hold on; plot((0:size(vx,2)-1)/fr + rollTx(1), vx)
 figure; plot((0:size(vx,2)-1)/fr + rollTx(1), [vx', vx_f'])
 fclose(fID);
 [~] = fclose(fID);
-figure; cwt(vx_f, 'amor', fr)
-[b9, a9] = butter(5, 18/fr, 'low');
-vx_f2 = filtfilt(b,a,vx);
+figure; cwt(vf, 'amor', fr)
+[b9, a9] = butter(5, (2*18)/fr, 'low');
+vf = filtfilt(b,a,vx);
 vx_f2 = filtfilt(b,a,double(vx));
 figure; plot((0:size(vx,2)-1)/fr + rollTx(1), [vx', vx_f', vx_f2'])
 figure; cwt(vx_f2, 'amor', fr)
