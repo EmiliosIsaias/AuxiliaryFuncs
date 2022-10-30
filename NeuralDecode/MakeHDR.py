c5cb8696cc5e642707f099b3f1a376c4f04b4bc2
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2022-10-30 23:04:58 +0100

    Create MakeHDR.py

---
+++ NeuralDecode/MakeHDR.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import cv2 as cv
+import numpy as np
+import pathlib as pl
+import matplotlib.pyplot as plt
+
+imgs_dir = pl.Path(r"D:\2022\10\29 Lab hike\\")
+imgs_nm = ["/_MG_00{}.png".format(x) for x in range(89,92)]
+#imgs_nm = ["/_MG_9209.png","/_MG_9210.png","/_MG_9211.png"]
+imgs_fn = [imgs_dir.as_posix() + im for im in imgs_nm]
+
+img_list = [cv.imread(fn) for fn in imgs_fn]
+
+alignMTB = cv.createAlignMTB()
+alignMTB.process(img_list, img_list)
+
+merge_mertens = cv.createMergeMertens()
+res_mertens = merge_mertens.process(img_list)
+
+res_mertens_8bit = np.clip(res_mertens*255, 0, 255).astype('uint8')
+plt.imshow(res_mertens_8bit)
+
+out_name = imgs_nm[0].partition(".")
+cv.imwrite(imgs_dir.as_posix() + out_name[0] + "_HDR.png", res_mertens_8bit)
