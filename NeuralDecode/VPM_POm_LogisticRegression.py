e3b470303ce7619cffa2dcd591ca4b1fda81b657
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-11-18 17:57:02 +0100

    Parallelising analysis

--- NeuralDecode/VPM_POm_LogisticRegression.py
+++ NeuralDecode/VPM_POm_LogisticRegression.py
@@ -8,15 +8,19 @@ Created on Fri Sep  9 14:13:30 2022
 from sklearn import linear_model, model_selection, metrics
 
 import matplotlib.pyplot as plt
+import matplotlib.figure as Fig
 import numpy as np
 import scipy.stats as sts
 from scipy import io
 import pathlib as pl
 
-data_path = pl.Path(r"E:\Database\vpm_pom_cluster_data_for_Emilio.mat")
-data = io.loadmat(data_path.as_posix())
+data_path = pl.Path(r"E:\Database")
+file_name = "vpm_pom_cluster_data_for_Emilio.mat"
+file_path = data_path.as_posix() + pl.os.sep + file_name
+data = io.loadmat(file_path)
 
-variable_info = io.whosmat(data_path.as_posix())
+
+variable_info = io.whosmat(file_path)
 variable_names = []
 for var_ in variable_info:
     variable_names.append(var_[0])
@@ -33,14 +37,16 @@ Yp = np.squeeze(data['is_pom'])
 # Puff and touch
 X = np.concatenate((sts.zscore(puffH, axis=1),
                     sts.zscore(touchH, axis=1)), axis=1)
-
+legStr = ("Puff", "Touch")
 """
 # Puff
 X = sts.zscore(puffH, axis=1)
+legStr = "Puff"
 """
 """
 # Touch
 X = sts.zscore(touchH, axis=1)
+legStr = "Touch"
 """
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(
@@ -49,12 +55,24 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(
     random_state=71)
 
 log_reg_model = linear_model.LogisticRegressionCV(
-    Cs=np.logspace(-3, 3, num=100), penalty='l2', solver='lbfgs')
+    Cs=np.logspace(-3, 3, num=600), penalty='l2', solver='lbfgs',  n_jobs=-1)
 
 log_reg_model.fit(X_train, y_train)
 
 tot_score, perm_scores, p = model_selection.permutation_test_score(
-    log_reg_model, X, Yv)
+    log_reg_model, X, Yv, n_permutations=500, verbose=True, n_jobs=-1)
 
 print("Total accuracy: {} | P: {} | Accuracy: {}".format(
     tot_score,p,metrics.accuracy_score(Yv, log_reg_model.predict(X))))
+
+# Plotting and saving results
+psth_tx = np.arange(-24.5, 75)
+coefs = log_reg_model.coef_
+if coefs.shape[1] > 100:
+    coefs = coefs.reshape([2,100])
+fig = plt.figure()
+plt.plot(psth_tx, coefs.transpose(), label=legStr)
+plt.legend()
+plt.xlabel("Time [ms]"); plt.ylabel("Coefficient magnitude")
+
+
