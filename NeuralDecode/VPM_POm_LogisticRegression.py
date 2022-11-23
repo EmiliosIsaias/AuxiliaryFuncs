72b015deacc5650866a894bd8a05bbf1deb01184
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-11-23 18:03:38 +0100

    Looped through the possible training

--- NeuralDecode/VPM_POm_LogisticRegression.py
+++ NeuralDecode/VPM_POm_LogisticRegression.py
@@ -33,46 +33,57 @@ Yv = np.squeeze(data['is_vpm'])
 Yp = np.squeeze(data['is_pom'])
 
 
-
+"""
 # Puff and touch
 X = np.concatenate((sts.zscore(puffH, axis=1),
                     sts.zscore(touchH, axis=1)), axis=1)
-legStr = ("Puff", "Touch")
-"""
+legStrs = (("Puff", "Touch"), "Puff", "Touch")
+title = ("Puff + Touch","Puff","Touch")
+
 # Puff
 X = sts.zscore(puffH, axis=1)
 legStr = "Puff"
+title = "Puff"
 """
 """
 # Touch
 X = sts.zscore(touchH, axis=1)
 legStr = "Touch"
+title = "Touch"
 """
+legStrs = (("Puff", "Touch"), "Puff", "Touch")
+title = ("Puff + Touch","Puff","Touch")
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(
-    X, Yv, 
-    test_size=0.15, 
-    random_state=71)
+def logReg(X, Yv, title_string, leg_string, test_size=0.15):
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(
+        X, Yv, test_size=test_size)
+    
+    log_reg_model = linear_model.LogisticRegressionCV(
+        Cs=np.logspace(-3, 3, num=300), penalty='l2', solver='lbfgs', 
+        n_jobs=-1)
+    
+    log_reg_model.fit(X_train, y_train)
+    
+    tot_score, perm_scores, p = model_selection.permutation_test_score(
+        log_reg_model, X, Yv, n_permutations=500, verbose=True, n_jobs=-1)
+    
+    print("Total accuracy: {} | P: {} | Accuracy: {}".format(
+        tot_score,p,metrics.accuracy_score(Yv, log_reg_model.predict(X))))
+    
+    # Plotting and saving results
+    psth_tx = np.arange(-24.5, 75)
+    coefs = log_reg_model.coef_
+    if coefs.shape[1] > 100:
+        coefs = coefs.reshape([2,100])
+    plt.figure()
+    plt.plot(psth_tx, coefs.transpose(), label=leg_string)
+    plt.legend()
+    plt.xlabel("Time [ms]"); plt.ylabel("Coefficient magnitude")
 
-log_reg_model = linear_model.LogisticRegressionCV(
-    Cs=np.logspace(-3, 3, num=600), penalty='l2', solver='lbfgs',  n_jobs=-1)
+for cx, X in enumerate((np.concatenate((sts.zscore(puffH, axis=1),
+                    sts.zscore(touchH, axis=1)), axis=1),
+                        sts.zscore(puffH, axis=1),
+                        sts.zscore(touchH, axis=1))):
+    logReg(X, Yv, title[cx], legStrs[cx])
 
-log_reg_model.fit(X_train, y_train)
-
-tot_score, perm_scores, p = model_selection.permutation_test_score(
-    log_reg_model, X, Yv, n_permutations=500, verbose=True, n_jobs=-1)
-
-print("Total accuracy: {} | P: {} | Accuracy: {}".format(
-    tot_score,p,metrics.accuracy_score(Yv, log_reg_model.predict(X))))
-
-# Plotting and saving results
-psth_tx = np.arange(-24.5, 75)
-coefs = log_reg_model.coef_
-if coefs.shape[1] > 100:
-    coefs = coefs.reshape([2,100])
-fig = plt.figure()
-plt.plot(psth_tx, coefs.transpose(), label=legStr)
-plt.legend()
-plt.xlabel("Time [ms]"); plt.ylabel("Coefficient magnitude")
-
-
+        
