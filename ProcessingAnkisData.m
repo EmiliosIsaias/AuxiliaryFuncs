76bec2478ea69f6c5d534bf7a4ed1896a4bfcd0d
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2023-02-24 14:08:15 +0100

    Added a population plot version

--- ProcessingAnkisData.m
+++ ProcessingAnkisData.m
@@ -3,8 +3,10 @@
 % dataPttrn = ...
 %     "Z:\Emilio\SuperiorColliculusExperiments\" + ...
 %     "Roller\Batch*_beh\WT*\*\*bar*";
-dataPttrn = "Z:\Emilio\SuperiorColliculusExperiments\Roller\"+...
-    "Batch13_beh\WT5\230214\*bar";
+% dataPttrn = "Z:\Emilio\SuperiorColliculusExperiments\Roller\"+...
+%     "Batch13_beh\WT*\*\*bar*";
+dataPttrn = "Z:\Emilio\SuperiorColliculusExperiments\Roller\" + ...
+    "Batch13_beh\WT3\230222\*bar";
 barFlds = dir(dataPttrn);
 fn = @(x) fullfile(x.folder, x.name);
 
@@ -47,14 +49,37 @@ for cf = barFlds'
     mice(mc).Sessions(sc).RollMovProb = ...
         [mice(mc).Sessions(sc).RollMovProb; ...
         outStr.Results(4).MovProbability];
-    close all
+    close('all')
 end
 
-%% Plotting data
+%% Plotting data all together
 piFig = figure("Name","Puff intensity vs Movement", "Color","w");
 ax = axes("Parent",piFig,"NextPlot","add");
 fnOpts = {'UniformOutput', false};
 lgOpts = {'Box', 'off', 'Location', 'best', 'NumColumnsMode', 'auto'};
+clrMap = lines(size(mice,1)); x = []; y = x; scObj = gobjects(size(mice));
+jittNoise = makedist("Normal","mu",0,"sigma",0.025);
+for mc = 1:size(mice,1)
+    for sc = 1:size(mice(mc).Sessions, 1)
+        x = [x; mice(mc).Sessions(sc).Intensities]; 
+        y = [y; mice(mc).Sessions(sc).RollMovProb];
+        scObj(mc) = scatter(ax, ...
+            random(jittNoise,size(mice(mc).Sessions(sc).Intensities)) + ...
+            mice(mc).Sessions(sc).Intensities, ...
+            random(jittNoise,size(mice(mc).Sessions(sc).Intensities))*0+...
+            mice(mc).Sessions(sc).RollMovProb, ".", ...
+            "SizeData", 108, "MarkerEdgeColor", clrMap(mc,:));
+    end
+end
+xlabel("Puff intensity [bar]"); ylabel("Movement probability");
+set(ax, "Box", "off", "Color", "none");
+[mdl, ~, ~] = polyfit(x, y, 1);
+lObj = plot(ax, [0;3],([0;3].^[0,1])*mdl', "k");
+lgObj = legend([scObj; lObj], ...
+    cat(1, arrayfun(@(x) x.Name, mice, "UniformOutput",false), 'Trend'));
+set(lgObj, "Box", "off", "Location", "best", "NumColumnsMode", "auto")
+%% Plotting data. One by one
+piFig = figure("Name","Puff intensity vs Movement", "Color","w");
 clrMap = lines(size(mice,1)); x = []; y = x; scObj = gobjects(size(mice));
 jittNoise = makedist("Normal","mu",0,"sigma",0.025);
 for mc = 1:size(mice,1)
