306dca8d37431f94a61f7bd207ae473f7ce0959d
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2023-06-30 18:08:45 +0200

    Create chi2test.m

---
+++ chi2test.m
@@ -0,0 +1,12 @@
+function [p, chiVal] = chi2test(contingencyTbl)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+total = sum(contingencyTbl, "all");
+sumCol = sum(contingencyTbl, 1);
+sumRow = sum(contingencyTbl, 2);
+expectedVal = (sumRow * sumCol)./total;
+chiVals = ((contingencyTbl - expectedVal).^2)./expectedVal;
+chiVal = sum(chiVals, "all");
+df = prod(size(contingencyTbl)-1);
+p = chi2pdf(chiVal, df);
+end
