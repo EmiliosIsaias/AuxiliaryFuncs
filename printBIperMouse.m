0a76789c12e09181e2026e09154caa6a912ede06
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2023-08-02 18:30:58 +0200

    Create printBIperMouse.m

---
+++ printBIperMouse.m
@@ -0,0 +1,44 @@
+function printBIperMouse(miceStr, cDir)
+
+Nm = numel(miceStr);
+Ns = arrayfun(@(m) numel(m.Sessions), miceStr);
+[~, fName] = fileparts(cDir);
+fPath = fullfile(cDir, fName);
+fPath = string(fPath)+".tsv";
+if exist(fPath, 'file')
+    fprintf(1, 'File exists!\nOverwrite?')
+    
+    return
+end
+fID = fopen(fPath, "w");
+if fID < 3
+    fprintf(1, 'Error creating the file!\n')
+    return
+end
+fprintf(fID, "Mouse\tInfo\tDate\t#Session\tConditions\n");
+for cm = 1:Nm
+    for cs = 1:Ns(cm)
+        tmpTbl = miceStr(cm).Sessions(cs).DataTable;
+        if strcmpi(miceStr(cm).Sessions(cs).Type, 'multi')
+            puffTbl = cellfun(@(x) any(contains(x, 'Puff')), ...
+                miceStr(cm).Sessions(cs).DataTable.Conditions);
+            tmpTbl = table(miceStr(cm).Sessions(cs).DataTable{puffTbl,1}{:}(:),...
+                miceStr(cm).Sessions(cs).DataTable{puffTbl,2}{:}(:), ...
+                'VariableNames', {'Conditions', 'BehaviourIndex'});
+        end
+        Nc = size(tmpTbl,1);
+        condBIs = "";
+        for cc = 1:Nc-1
+            condBIs = condBIs + sprintf("%s\t%.3f\t", ...
+                tmpTbl{cc,:});
+        end
+        condBIs = condBIs + sprintf("%s\t%.3f\n", ...
+            tmpTbl{Nc,:});
+        % Writing mouse, 'structure', session date, session number, and conditions
+        fprintf(fID, "%s\t%s\t%s\t%d\t%s", miceStr(cm).Name, ...
+            miceStr(cm).Structure, miceStr(cm).Sessions(cs).Date, ...
+            condBIs);
+    end
+end
+fclose(fID);
+end
