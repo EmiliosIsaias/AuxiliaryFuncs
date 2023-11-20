67b7311688323bab403d2d43088085ad67a634e7
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2023-11-20 17:53:19 +0100

    Tests for VPM-POm binomial GLM

--- R/VPM_POm_BinomialGLM.R
+++ R/VPM_POm_BinomialGLM.R
@@ -5,9 +5,22 @@ mdata <- R.matlab::readMat(
   "C:\\Users\\neuro\\seadrive_root\\Emilio U\\My Libraries\\My Library\\VPM-POm spatial correlation\\vpm_pom_cluster_data_for_Emilio_Oct_23.mat"
   )
 
-td2 <- list( vpm = mdata$is.vpm[,1], 
-            PF =  matrix( standardize( mdata$Puff.PSTH ) , nrow = 28 ) ,
-            TC =  matrix( standardize( mdata$Touch.PSTH ) , nrow = 28 )  )
+mdata2 <- R.matlab::readMat(
+  "C:\\Users\\neuro\\seadrive_root\\Emilio U\\My Libraries\\My Library\\VPM-POm spatial correlation\\vpm_pom_cluster_data_for_Emilio.mat"
+)
+
+# td <- list( vpm = mdata$is.vpm[,1],
+#             PF =  matrix( standardize( mdata$Puff.PSTH ) , nrow = nrow(mdata$Puff.PSTH) ) ,
+#             TC =  matrix( standardize( mdata$Touch.PSTH ) , nrow = nrow(mdata$Touch.PSTH) )  )
+
+td <- list( vpm = mdata$is.vpm[,1],
+            PF =  mdata$Puff.PSTH / sapply(1:nrow(mdata$Puff.PSTH), function(i) max(mdata$Puff.PSTH[i,] ) ) ,
+            TC =  mdata$Touch.PSTH / sapply(1:nrow(mdata$Touch.PSTH), function(i) max(mdata$Touch.PSTH[i,] ) ) )
+
+
+td2 <- list( vpm = mdata2$is.vpm[,1],
+             PF =  matrix( standardize( mdata2$Puff.PSTH ) , nrow = nrow(mdata2$Puff.PSTH) ) ,
+             TC =  matrix( standardize( mdata2$Touch.PSTH ) , nrow = nrow(mdata2$Touch.PSTH) )  )
 
 mth.binom <- quap(
   alist(
@@ -41,7 +54,17 @@ mth.pt <- quap(
     A ~ dnorm(0, 3),
     B ~ dnorm(0, 5),
     W ~ dnorm(0, 5) ), 
-  data = td2,   
+  data = td,   
+  start = list( B = rep(0, ncol(td2$PF) ),
+                W = rep(0, ncol(td2$TC) ) ) )
+
+mth.pta <- quap(
+  alist(
+    vpm ~ dbinom(1, p),
+    logit(p) <- PF %*% B + TC %*% W,
+    B ~ dnorm(0, 5),
+    W ~ dnorm(0, 5) ), 
+  data = td,   
   start = list( B = rep(0, ncol(td2$PF) ),
                 W = rep(0, ncol(td2$TC) ) ) )
 
@@ -58,10 +81,10 @@ mth.pt.u <- ulam(
   chains = 4, 
   cores = 4)
 
-N <- 1e4
+N <- 1e5
 post.pt <- extract.samples(mth.pt, n = N)
 pred.pt <- inv_logit( 
-  mean(post.pt$A) + 
+    mean(post.pta$A) +
     td2$PF %*% apply(post.pt$B, 2, mean) + 
     td2$TC %*% apply(post.pt$W, 2, mean) )
 pred.pt.post <- sapply(1:N, 
@@ -73,22 +96,24 @@ pred.pt.post.p <- inv_logit(pred.pt.post)
 
 vpm.pred.md <- apply(pred.pt.post.p, 1, median)
 vpm.pred.mu <- apply(pred.pt.post.p, 1, mean)
-library(devEMF)
-emf(
-  file = "C:\\Users\\neuro\\seadrive_root\\Emilio U\\My Libraries\\My Library\\VPM-POm spatial correlation\\VPM likelihood.emf",
-  emfPlus = TRUE,
-  bg = "transparent",
-  fg = "black",
-  emfPlusRaster = TRUE
-  )
-plot(NULL, xlim = c(0,29), ylim = c(-0.1, 1.1), ylab="VPM likelihood", 
+# library(devEMF)
+# emf(
+#   file = "C:\\Users\\neuro\\seadrive_root\\Emilio U\\My Libraries\\My Library\\VPM-POm spatial correlation\\VPM likelihood1.emf",
+#   emfPlus = TRUE,
+#   bg = "transparent",
+#   fg = "black",
+#   emfPlusRaster = TRUE
+#   )
+plot(NULL, xlim = c(0,length(vpm.pred.mu)+1), ylim = c(-0.1, 1.1), ylab="VPM likelihood", 
      xlab="Cell index")
-lines(1:28, vpm.pred.md, lwd=2, type = "l", col="black")
+lines(1:length(vpm.pred.mu), vpm.pred.md, lwd=2, type = "l", col="black")
 for (i in c(0.75,0.5,0.25,0.1,0.05)) {
-  shade(apply(pred.pt.post.p, 1, PI, prob = i), 1:28, col = col.alpha("black", 0.3))
+  shade(apply(pred.pt.post.p, 1, PI, prob = i), 
+        1:length(vpm.pred.mu), 
+        col = col.alpha("black", 0.3))
 }
-lines(1:28, vpm.pred.mu, lwd=3, type="l", col=rangi2)
-dev.off()
+lines(1:length(vpm.pred.mu), vpm.pred.mu, lwd=3, type="l", col=rangi2)
+# dev.off()
 
 mth.p <- quap(
   alist(
