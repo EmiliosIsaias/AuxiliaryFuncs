2528f69a3dd842a6db0cd3f34373d1360687ad18
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2023-12-04 18:23:18 +0100

    Getting closer to sensibly pool all data

--- summariseMiceBeh.m
+++ summariseMiceBeh.m
@@ -3,10 +3,12 @@ function [behTable, miceStruct] = summariseMiceBeh(miceStruct)
 fnOpts = {'UniformOutput', false};
 dlyPttrn = 'delay\ \d?.\d+\ s$';
 dlfPttrn = [dlyPttrn(1:end-1), '\ \+\ L\d+.\d?'];
-rxOpts = {'once', 'match', 'freespacing'};
+rxOpts = {'once', 'freespacing'};
 
 snglFlag = arrayfun(@(m) arrayfun(@(s) string(s.Type) == "single", ...
     m.Sessions), miceStruct, fnOpts{:});
+
+[expTypes, ~, expTypeMbrshp] = unique([miceStruct.Structure]);
 
 multiSess = cellfun(@(x) find(~x), snglFlag, fnOpts{:});
 multiMice = find(cellfun(@(x) ~isempty(x), multiSess));
@@ -40,22 +42,62 @@ Ncpspm = arrayfun(@(m) arrayfun(@(s) size(s.DataTable,1), ...
 
 behTable = arrayfun(@(m) arrayfun(@(s) {s.DataTable}, m.Sessions), ...
     miceStruct, fnOpts{:});
-
-[expTypes, ~, expTypeMbrshp] = unique([miceStruct.Structure]);
-snglFlag = arrayfun(@(m) arrayfun(@(s) string(s.Type) == "single", ...
-    m.Sessions), miceStruct, fnOpts{:});
-
 allCondNames = cellfun(@(m) cellfun(@(t) {t.Conditions}, m), ...
     behTable, fnOpts{:});
 
 allCondNames_aux = cellfun(@(x) cat(1, x{:}), allCondNames, fnOpts{:});
 allCondNames_aux = cat(1, allCondNames_aux{:});
-[uallCondNames_aux, ~, acnSubs] = unique(allCondNames_aux);
+[uallCondNames_aux, ~, acnSubs] = unique(allCondNames_aux(:));
 dels = arrayfun(@(x) textscan(x, 'Delay %.3f s'), uallCondNames_aux);
+ctrlCond = contains(uallCondNames_aux, 'control', 'IgnoreCase', true);
 [udels, ~, udSubs] = uniquetol([dels{:}], 0.1 / max([dels{:}]));
+udSubs2 = nan(size(dels)); udSubs2(~cellfun(@isempty, dels)) = udSubs;
+
+condMembership = [allCondNames_aux, acnSubs, udSubs2(acnSubs)];
+% Assuming the same frequency for all
+frqs = arrayfun(@(x) regexpi(x, dlfPttrn, rxOpts{:}), uallCondNames_aux,...
+    fnOpts{:}); frqs = ~cellfun(@isempty, frqs); 
+frqSubs = find(frqs, 1, "first");
+pairedStimSubs = [udSubs2, frqs];
+[delAndFrq, ~, dfSubs] = unique(pairedStimSubs, 'rows');
+dfSubs(isnan(udSubs2)) = NaN; delAndFrq(any(isnan(delAndFrq),2),:) = [];
 
 
-behTable = arrayfun(@(m) arrayfun(@(s) {s.DataTable}, m.Sessions), ...
-    miceStruct, fnOpts{:});
-
+%% Experimental group
+idxTh = cumsum(cellfun(@sum, Ncpspm));
+mscSubs = zeros(sum(cellfun(@sum,Ncpspm)), 4, "single");
+xmice = struct('ExperimentalGroup', {expTypes{:}}, ...
+    'DataTable', [], ...
+    'MiceNames',[], ...
+    'SessionDate', []);
+cidx = 1;
+for ce = 1:numel(expTypes)
+    % Experimental group
+    etpcIdx = pairedStimSubs(acnSubs(cidx:idxTh(find(expTypeMbrshp==ce, ...
+        1,'last'))),:);
+end
+%%
+idxTh = cumsum(cellfun(@sum, Ncpspm));
+mscSubs = zeros(sum(cellfun(@sum,Ncpspm)), 4, "single");
+xmice = struct('ExperimentalGroup', {expTypes{:}}, ...
+    'DataTable', [], ...
+    'MiceNames',[], ...
+    'SessionDate', []);
+cidx = 1;
+for cm = 1:Nm
+    % Mouse
+    for cs = 1:Nspm(cm)
+        % Session
+        pairedStimSubs(acnSubs(cidx:idxTh(find(expTypeMbrshp==1,1,'last'))),:)
+        udSubs2(acnSubs(cidx:idxTh(cm)))
+        dfSubs(acnSubs(cidx:idxTh(cm)))
+        allCondNames_aux(cidx:idxTh(cm))
+        for cc = 1:Ncpspm{cm}(cs)
+            % Condition
+            
+            mscSubs(idx,:) = [expTypeMbrshp(cm), cm, cs, cc];
+            idx = idx + 1;
+        end
+    end
+end
 end
