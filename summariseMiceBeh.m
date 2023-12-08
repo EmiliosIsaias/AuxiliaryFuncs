cb4d5256ac8749c3f4702b5d3120353d75277054
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2023-12-08 18:25:02 +0100

    Created the BI matrix per mouse

--- summariseMiceBeh.m
+++ summariseMiceBeh.m
@@ -35,7 +35,7 @@ for cm = multiMice(:)'
         
     end
 end
-
+%% Grouping condition names across all mice
 Nm = numel(miceStruct); Nspm = arrayfun(@(m) numel(m.Sessions), miceStruct);
 Ncpspm = arrayfun(@(m) arrayfun(@(s) size(s.DataTable,1), ...
     m.Sessions), miceStruct, fnOpts{:});
@@ -44,7 +44,7 @@ behTable = arrayfun(@(m) arrayfun(@(s) {s.DataTable}, m.Sessions), ...
     miceStruct, fnOpts{:});
 allCondNames = cellfun(@(m) cellfun(@(t) {t.Conditions}, m), ...
     behTable, fnOpts{:});
-
+asCondNames = cellfun(@(m) cat(1, m{:}), allCondNames, fnOpts{:});
 allCondNames_aux = cellfun(@(x) cat(1, x{:}), allCondNames, fnOpts{:});
 allCondNames_aux = cat(1, allCondNames_aux{:});
 [uallCondNames_aux, ~, acnSubs] = unique(allCondNames_aux(:));
@@ -52,58 +52,58 @@ dels = arrayfun(@(x) textscan(x, 'Delay %.3f s'), uallCondNames_aux);
 ctrlCond = contains(uallCondNames_aux, 'control', 'IgnoreCase', true);
 [udels, ~, udSubs] = uniquetol([dels{:}], 0.1 / max([dels{:}]));
 udSubs2 = nan(size(dels)); udSubs2(~cellfun(@isempty, dels)) = udSubs;
-udSubs2(ctrlCond) = 0;
+udSubs2(ctrlCond) = 0; udSubs2(isnan(udSubs2)) = find(isnan(udSubs2));
 
-condMembership = [allCondNames_aux, acnSubs, udSubs2(acnSubs)];
+
 % Assuming the same frequency for all
 frqs = arrayfun(@(x) regexpi(x, dlfPttrn, rxOpts{:}), uallCondNames_aux,...
     fnOpts{:}); frqs = ~cellfun(@isempty, frqs); 
 frqSubs = find(frqs, 1, "first");
 pairedStimSubs = [udSubs2, frqs];
-[delAndFrq, ~, dfSubs] = unique(pairedStimSubs, 'rows');
-dfSubs(isnan(udSubs2)) = NaN; delAndFrq(any(isnan(delAndFrq),2),:) = [];
+[~, ~, dfSubs] = unique(pairedStimSubs, 'rows');
+dfSubs(isnan(udSubs2)) = NaN; % delAndFrq(any(isnan(delAndFrq),2),:) = [];
 
+condMembership = [allCondNames_aux, acnSubs, udSubs2(acnSubs), single(frqs(acnSubs))];
+
+sessNames = cell(Nm, 1);
+
+miceNames = cat(1, miceStruct.Name);
+miceMats = cell(Nm, 1);
+idxTh = cumsum(cellfun(@sum, Ncpspm)); cidx = 1;
+for cm = 1:Nm
+    mSessNames = string({miceStruct(cm).Sessions.Date})';
+    sessNames{cm} = mSessNames;
+    mBI = arrayfun(@(s) s.DataTable.BehaviourIndices, ...
+        miceStruct(cm).Sessions, fnOpts{:}); mBI = cat(1, mBI{:});
+    mCondClass = dfSubs(acnSubs(cidx:idxTh(cm)));
+    cidx = idxTh(cm) + 1;
+    [uAux, ~, muc] = unique(mCondClass); Ncpm_as = numel(uAux);
+    mouseMat = nan(Ncpm_as, Nspm(cm));
+    cidx2 = 1; 
+    for cc = 1:Nspm(cm)
+        cidx3 = cidx2:cidx2-1+Ncpspm{cm}(cc);
+        sIdx = muc(cidx3);
+        mouseMat(sIdx, cc) = mBI(cidx3);
+        cidx2 = cidx2 + Ncpspm{cm}(cc);
+    end
+    miceMats{cm} = mouseMat';
+end
+
+Nc_s = cellfun(@size, miceMats, fnOpts{:});
+mxC = arrayfun(@(x) max(cat(1, Nc_s{expTypeMbrshp == x}),[],1), ...
+    unique(expTypeMbrshp), fnOpts{:});
 
 %% Experimental group
-idxTh = cumsum(cellfun(@sum, Ncpspm));
-mscSubs = zeros(sum(cellfun(@sum,Ncpspm)), 4, "single");
 xmice = struct('ExperimentalGroup', {expTypes{:}}, ...
     'DataTable', [], ...
-    'MiceNames',[], ...
+    'MiceNames', arrayfun(@(x) cat(1, miceNames(expTypeMbrshp == x)), ...
+    1:numel(expTypes), fnOpts{:}), ...
     'SessionDate', []);
-cidx = 1;
-for ce = 1:numel(expTypes)
-    % Experimental group
-    cx = find(expTypeMbrshp == ce, 1,'last');
-    exIdx = acnSubs(cidx:idxTh(cx));
-    etpcIdx = pairedStimSubs(exIdx,:);
-    condType = dfSubs(exIdx);
-    
+condNames = cell(numel(expTypes), 1);
 
-    cidx = idxTh(cx) + 1;
+for ce = 1:numel(expTypes)
+    xmice(ce).DataTable = nan(sum(expTypeMbrshp == ce), mxC{ce}(1), mxC{ce}(2));
+
 end
-% %%
-% idxTh = cumsum(cellfun(@sum, Ncpspm));
-% mscSubs = zeros(sum(cellfun(@sum,Ncpspm)), 4, "single");
-% xmice = struct('ExperimentalGroup', {expTypes{:}}, ...
-%     'DataTable', [], ...
-%     'MiceNames',[], ...
-%     'SessionDate', []);
-% cidx = 1;
-% for cm = 1:Nm
-%     % Mouse
-%     for cs = 1:Nspm(cm)
-%         % Session
-%         pairedStimSubs(acnSubs(cidx:idxTh(find(expTypeMbrshp==1,1,'last'))),:)
-%         udSubs2(acnSubs(cidx:idxTh(cm)))
-%         dfSubs(acnSubs(cidx:idxTh(cm)))
-%         allCondNames_aux(cidx:idxTh(cm))
-%         for cc = 1:Ncpspm{cm}(cs)
-%             % Condition
-%             
-%             mscSubs(idx,:) = [expTypeMbrshp(cm), cm, cs, cc];
-%             idx = idx + 1;
-%         end
-%     end
-% end
+
 end
