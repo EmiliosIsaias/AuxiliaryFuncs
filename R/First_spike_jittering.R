e69f79b65b4ba00ed626fb3380fe4087575315e8
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-02-08 16:46:01 +0100

    All units with spikes within 0 and 50 ms

--- R/First_spike_jittering.R
+++ R/First_spike_jittering.R
@@ -16,28 +16,21 @@ d <- list(
 mFSJ.f <- ulam(
   alist(
     Fs ~ normal( mu, sigma ),
-    # tid == 1 is the neuron with it's natural latency and trial-to-trial variance
+    
     mu <- latency[neuron] + mod_lat[tid],
-    log(sigma) <- jitt[neuron] + mod_jit[tid],
+    log(sigma) <- jit[neuron] + mod_jit[tid],
     
-    #Adaptive, non-centered priors
-    transpars> matrix[neuron,6]:latency <-
-      compose_noncentered( sigma_latency , L_Rho_latency , z_latency ),
-    matrix[6,neuron]:z_latency ~ normal( 0 , 1 ),
+    vector[164]:latency ~ normal( pop_lat, pop_lsigma),
+    pop_lat ~ normal(0, 1),
+    pop_lsigma ~ exponential( 2 ),
     
-    transpars> matrix[neuron,6]:jitt <-
-      compose_noncentered( sigma_jitt , L_Rho_jitt , z_jitt ),
-    matrix[6,neuron]:z_jitt ~ normal( 0 , 1 ),
+    vector[164]:jit ~ normal( pop_jit, pop_jsigma),
+    pop_jit ~ normal(0, 1),
+    pop_jsigma ~ exponential( 2 ),
     
-    #Fixed priors
-    vector[6]:sigma_latency ~ exponential( 1 ),
-    cholesky_factor_corr[6]:L_Rho_latency ~ lkj_corr_cholesky( 2 ),
-    vector[6]:sigma_jitt ~ exponential( 2 ),
-    cholesky_factor_corr[6]:L_Rho_jitt ~ lkj_corr_cholesky( 2 ),
+    mod_lat[tid] ~ normal( 0, 1.5),
+    mod_jit[tid] ~ exponential( 2 )
     
-    #Finally, compute ordinary correlation matrices from Cholesky factors
-    gq> matrix[6,6]:Rho_latency <<- Chol_to_Corr( L_Rho_latency ),
-    gq> matrix[6,6]:Rho_jitt <<- Chol_to_Corr( L_Rho_jitt )
   ), data = d , chains = 4, cores = 4)
 
 mFSJ <- ulam(
@@ -70,4 +63,4 @@ mFSJ <- ulam(
 post <- link( mFSJ )
 params <- extract.samples( mFSJ )
 
-R.matlab::writeMat("Z:\\Jesus\\Jittering\\FULLYCurated\\05_190701_Jesus_Emili_Jittering_3800_1600_1500 yes VPM good\\KS2_newChanMap\\Jittering_BI.mat", post = post, params = params)
+R.matlab::writeMat("Z:\\Jesus\\Jittering\\FULLYCurated\\05_190701_Jesus_Emili_Jittering_3800_1600_1500 yes VPM good\\KS2_newChanMap\\Jittering_BI_filtered.mat", post = post, params = params)
