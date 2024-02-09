8fb0b0a4fa6c53114923f297dd5d0370947e19e9
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2024-02-09 17:28:44 +0100

    Working model, need improvements

--- R/First_spike_jittering.R
+++ R/First_spike_jittering.R
@@ -1,7 +1,7 @@
 library(rethinking)
 library(R.matlab)
 
-fpath <- "Z:\\Jesus\\Jittering\\FULLYCurated\\05_190701_Jesus_Emili_Jittering_3800_1600_1500 yes VPM good\\KS2_newChanMap\\FirstSpikes_perUnit_and_condition_filtered.mat"
+fpath <- "Z:\\Jesus\\Jittering\\FULLYCurated\\05_190701_Jesus_Emili_Jittering_3800_1600_1500 yes VPM good\\KS2_newChanMap\\fs_puc_respFilt_tmFilt.mat"
 
 dat <- R.matlab::readMat(fpath)
 
@@ -13,25 +13,58 @@ d <- list(
   neuron = as.integer( dat$fs.puc[,2] )
   )
 
-mFSJ.f <- ulam(
+mFSJ.f1 <- ulam(
   alist(
     Fs ~ normal( mu, sigma ),
     
-    mu <- latency[neuron] + mod_lat[tid],
-    log(sigma) <- jit[neuron] + mod_jit[tid],
+    mu <- g[tid] + alpha[neuron,tid],
     
-    vector[164]:latency ~ normal( pop_lat, pop_lsigma),
-    pop_lat ~ normal(0, 1),
-    pop_lsigma ~ exponential( 2 ),
+    transpars> matrix[neuron,6]:alpha <- 
+      compose_noncentered( sigma_alpha, L_Rho_alpha, z_alpha),
+    matrix[6,neuron]:z_alpha ~ normal(0, 1),
+    vector[6]:sigma_alpha ~ exponential( 1 ),
+    cholesky_factor_corr[6]:L_Rho_alpha ~ lkj_corr_cholesky(3),
     
-    vector[164]:jit ~ normal( pop_jit, pop_jsigma),
-    pop_jit ~ normal(0, 1),
-    pop_jsigma ~ exponential( 2 ),
+    g[tid] ~ normal(0, 1),
+    sigma ~ exponential( 1 ),
     
-    mod_lat[tid] ~ normal( 0, 1.5),
-    mod_jit[tid] ~ exponential( 2 )
-    
+    gq> matrix[6,6]:Rho_alpha <<- Chol_to_Corr(L_Rho_alpha)
   ), data = d , chains = 4, cores = 4)
+
+mFSJ.f1_stan <- "data{
+     vector[5912] Fs;
+    array[5912] int neuron;
+    array[5912] int tid;
+}
+parameters{
+     matrix[6,164] z_alpha;
+     vector<lower=0>[6] sigma_alpha;
+     cholesky_factor_corr[6] L_Rho_alpha;
+     vector[6] g;
+     real<lower=0> sigma;
+}
+transformed parameters{
+     matrix[164,6] alpha;
+    alpha = (diag_pre_multiply(sigma_alpha, L_Rho_alpha) * z_alpha)';
+}
+model{
+     vector[5912] mu;
+    sigma ~ exponential( 1 );
+    g ~ normal( 0 , 1 );
+    L_Rho_alpha ~ lkj_corr_cholesky( 3 );
+    sigma_alpha ~ exponential( 1 );
+    to_vector( z_alpha ) ~ normal( 0 , 1 );
+    for ( i in 1:5912 ) {
+        mu[i] = g[tid[i]] + alpha[neuron[i], tid[i]];
+    }
+    Fs ~ normal( mu , sigma );
+}
+generated quantities{
+     matrix[6,6] Rho_alpha;
+    Rho_alpha = multiply_lower_tri_self_transpose(L_Rho_alpha);
+}
+"
+mFSJ.f1 <- stan( model_code=mFSJ.f1_stan , data=d , chains=4 , cores=4 )
 
 mFSJ <- ulam(
   alist(
@@ -60,7 +93,11 @@ mFSJ <- ulam(
     gq> matrix[6,6]:Rho_jitt <<- Chol_to_Corr( L_Rho_jitt )
   ), data = d , chains = 4, cores = 4)
 
-post <- link( mFSJ )
-params <- extract.samples( mFSJ )
+post <- link( mFSJ.f1 )
+params <- extract.samples( mFSJ.f1 )
 
-R.matlab::writeMat("Z:\\Jesus\\Jittering\\FULLYCurated\\05_190701_Jesus_Emili_Jittering_3800_1600_1500 yes VPM good\\KS2_newChanMap\\Jittering_BI_filtered.mat", post = post, params = params)
+fs_att <- attributes(Z_fs)
+fs_centre <- fs_att$`scaled:center`
+fs_scale <- fs_att$`scaled:scale`
+
+R.matlab::writeMat("C:\\Users\\jefe_\\mFSJ_f1_mu_respFilt_tmFilt.mat", post = post, params = params, fs_centre = fs_centre, fs_scale = fs_scale)
