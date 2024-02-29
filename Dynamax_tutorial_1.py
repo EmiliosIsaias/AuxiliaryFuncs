aaa83760ae53a95c34ff30543fad17bd3fa86ad7
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-02-29 18:35:22 +0100

    Trying dynamax for LFP

--- Dynamax_tutorial_1.py
+++ Dynamax_tutorial_1.py
@@ -8,16 +8,32 @@ import jax.numpy as jnp
 import jax.random as jr
 import matplotlib.pyplot as plt
 from dynamax.hidden_markov_model import GaussianHMM
+from scipy import io
+import pathlib as pl
 
-key1, key2, key3 = jr.split(jr.PRNGKey(0), 3)
-num_states = 3
-emission_dim = 2
-num_timesteps = 1000
+import numpy as np
+from itertools import count
+
+fpath = pl.Path(r"Z:\Jesus\Jittering\FULLYCurated\05_190701_Jesus_Emili_Jittering_3800_1600_1500 yes VPM good\KS2_newChanMap\LFP_4_R.mat")
+dat = io.loadmat(fpath)
+
+y =  jnp.array( dat['lfp_z'] )
+N = np.squeeze( dat['N'] )
+
+keys = map(jr.PRNGKey, count())
+num_states = 2
+emission_dim = 1
+num_timesteps = N
+
+hmm = GaussianHMM(num_states, emission_dim)
+params, props = hmm.initialize(next(keys), emission_means=(-3,2))
+params, lps = hmm.fit_em(params, props, y )
+
 
 # Make a Gaussian HMM and sample data from it
 hmm = GaussianHMM(num_states, emission_dim)
-true_params, _ = hmm.initialize(key1)
-true_states, emissions = hmm.sample(true_params, key2, num_timesteps)
+true_params, _ = hmm.initialize(next(keys))
+true_states, emissions = hmm.sample(true_params, next(keys), num_timesteps)
 
 # Make a new Gaussian HMM and fit it with EM
 params, props = hmm.initialize(key3, method="kmeans", emissions=emissions)
