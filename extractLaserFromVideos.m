ce247ec7e31da50a869da4b1876b185a3a1f49d2
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-04-22 19:24:49 +0200

    Prototype for a function

--- extractLaserFromVideos.m
+++ extractLaserFromVideos.m
@@ -7,23 +7,27 @@ repo_paths = cellfun(@(x) char( fullfile( home_path, x) ), ...
 addpath( repo_paths{:} )
 %%
 expandPath = @(x) fullfile( x.folder, x.name);
-exp_path = getParentDir( pwd, 1);
+beh_path = pwd;
+fprintf( 1, "Working directory: %s\n", beh_path )
+% beh_path = "Z:\Emilio\SuperiorColliculusExperiments\Roller\Batch18_ephys\iRNs\GADi39\240206_C+F_2399\Behaviour";
+exp_path = getParentDir( beh_path, 1);
+
 m = 1e-3;
 
-video_paths = dir( fullfile( exp_path, "Behavio*", "roller*.avi" ) );
+video_paths = dir( fullfile( beh_path, "roller*.mp4" ) );
 vidObj = arrayfun(@(x) VideoReader( expandPath( x ) ), ...
     video_paths, fnOpts{:} );
 
-readCSV = @(x) readtable(x, "Delimiter", ",");
-fid_paths = dir( "FrameID*.csv" );
+readCSV = @(x) readtable(x, "Delimiter", ",");  
+fid_paths = dir( fullfile( beh_path, "FrameID*.csv" ) );
 
 vidTx = arrayfun(@(x) readCSV( expandPath( x ) ), fid_paths, fnOpts{:} );
 vidTx = cellfun(@(x) x.Var2 ./ 1e9, vidTx, fnOpts{:} ); % nanoseconds
 
-dlc_paths = dir( fullfile( exp_path, "Behavio*", "roller*filtered.csv" ) );
-fid_paths = dir( fullfile( exp_path, "Behavio*", "FrameID*.csv" ) );
+dlc_paths = dir( fullfile( beh_path, "roller*filtered.csv" ) );
+fid_paths = dir( fullfile( beh_path, "FrameID*.csv" ) );
 
-tf_paths = dir( fullfile( exp_path, "Behavio*", "TriggerSignals*.bin") );
+tf_paths = dir( fullfile( beh_path, "TriggerSignals*.bin") );
 fsf_path = dir( fullfile( exp_path, "ephys*", "*_sampling_frequency.mat") );
 
 if isempty( tf_paths )
@@ -31,8 +35,10 @@ if isempty( tf_paths )
 end
 
 if isempty( fsf_path )
-    fsf_path = dir( fullfile( exp_path, "Behavio*", "*_sampling_frequency.mat") );
+    fsf_path = dir( fullfile( beh_path, "*_sampling_frequency.mat") );
 end
+
+fprintf(1, "Sampling frequency file: %s\n", fsf_path.name)
 
 fs_ephys = load( expandPath( fsf_path ), "fs" ); fs_ephys = fs_ephys.fs;
 Ns_intan = [tf_paths.bytes]' ./ 4; % 2 signals x 2 bytes per sample.
@@ -44,9 +50,9 @@ dlcTables = arrayfun(@(x) readDLCData(expandPath(x)), ...
     dlc_paths, fnOpts{:});
 
 delta_tiv = Texp_ephys - Texp_vid;
-
+%% 
 lsrInt = cellfun(@(v,t) getLaserIntensitySignalFromVideo(v, t), ...
     vidObj(:), dlcTables(:), fnOpts{:} );
 
-save( fullfile( video_paths(1).folder, "VideoLaserIntensity.mat" ), ...
+save( fullfile( beh_path, "VideoLaserIntensity.mat" ), ...
     "lsrInt", "delta_tiv", "Texp_vid", "Texp_ephys")
