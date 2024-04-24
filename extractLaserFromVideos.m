119f0125a7c1bbb713b993abed186d16408b9664
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-04-24 15:36:14 +0200

    Functionalised the script

--- extractLaserFromVideos.m
+++ extractLaserFromVideos.m
@@ -1,20 +1,15 @@
+function [lsrInt] = extractLaserFromVideos( beh_path ) 
 
 fnOpts = {'UniformOutput', false};
-
-home_path = '/gpfs/bwfor/home/hd/hd_hd/hd_bf154/';
-repo_paths = cellfun(@(x) char( fullfile( home_path, x) ), ...
-    {'NeuroNetzAnalysis', 'AuxiliaryFuncs', 'Scripts'},  fnOpts{:} );
-addpath( repo_paths{:} )
-%%
 expandPath = @(x) fullfile( x.folder, x.name);
-beh_path = pwd;
+
 fprintf( 1, "Working directory: %s\n", beh_path )
-% beh_path = "Z:\Emilio\SuperiorColliculusExperiments\Roller\Batch18_ephys\iRNs\GADi39\240206_C+F_2399\Behaviour";
+
 exp_path = getParentDir( beh_path, 1);
 
 m = 1e-3;
 
-video_paths = dir( fullfile( beh_path, "roller*.mp4" ) );
+video_paths = dir( fullfile( beh_path, "roller*.avi" ) );
 vidObj = arrayfun(@(x) VideoReader( expandPath( x ) ), ...
     video_paths, fnOpts{:} );
 
@@ -50,9 +45,11 @@ dlcTables = arrayfun(@(x) readDLCData(expandPath(x)), ...
     dlc_paths, fnOpts{:});
 
 delta_tiv = Texp_ephys - Texp_vid;
-%% 
+
 lsrInt = cellfun(@(v,t) getLaserIntensitySignalFromVideo(v, t), ...
     vidObj(:), dlcTables(:), fnOpts{:} );
 
 save( fullfile( beh_path, "VideoLaserIntensity.mat" ), ...
     "lsrInt", "delta_tiv", "Texp_vid", "Texp_ephys")
+
+end
