7de931be11a0da32f1febe76301f8c9680861bd9
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-05-03 17:12:14 +0200

    Cross-platform

--- extract_uncertain_frames.py
+++ extract_uncertain_frames.py
@@ -4,16 +4,25 @@ Created on Fri May  3 11:10:19 2024
 
 @author: neuro
 """
+import platform
 import deeplabcut as dlc
 import pathlib as pl
 
-config_path = pl.Path(r'Z:/Emilio\\AwakenSC-EIC-2021-07-20\\config.yaml')
+os_name = platform.system()
+
+if os_name == 'Linux':
+    path_base = '/mnt/sds-hd/sd19b001/'
+else:
+    path_base = 'Z:\\';
+
+
+config_path = pl.Path(path_base + 'Emilio/AwakenSC-EIC-2021-07-20/config.yaml')
 config_path = config_path.as_posix()
 
-batch_path = pl.Path(r'Z:\Emilio\SuperiorColliculusExperiments\Roller\Batch10_ephys.e\RNs\WTg63\221109_PTX100microM_2000\Behaviour')
+batch_path = pl.Path(r'./')
 
 video_paths = list( batch_path.glob(r'**/*.avi') )
 video_paths = [v.as_posix() for v in video_paths]
 
 dlc.extract_outlier_frames( config_path, video_paths, 
-                           outlieralgorithm='uncertain', p_bound=0.7 )
+                           outlieralgorithm='uncertain', p_bound=0.5 )
