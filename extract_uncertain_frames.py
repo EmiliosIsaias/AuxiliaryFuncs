b5578182d2338335628faa605b784ea6f5c01aa0
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-05-03 11:30:47 +0200

    Path to string

--- extract_uncertain_frames.py
+++ extract_uncertain_frames.py
@@ -10,10 +10,10 @@ import pathlib as pl
 config_path = pl.Path(r'Z:/Emilio\\AwakenSC-EIC-2021-07-20\\config.yaml')
 config_path = config_path.as_posix()
 
-batch_path = pl.Path(
-    r'Z:\Emilio\SuperiorColliculusExperiments\Roller\Batch6_beh+Muscimol\Musc\ChR47\220531\Behaviour')
+batch_path = pl.Path(r'Z:\Emilio\SuperiorColliculusExperiments\Roller\Batch6_beh+Muscimol\Musc\ChR47\220531\Behaviour')
 
-video_paths = list( batch_path.glob('**/*.avi') )
+video_paths = list( batch_path.glob(r'**/*.avi') )
+video_paths = [v.as_posix() for v in video_paths]
 
 dlc.extract_outlier_frames( config_path, video_paths, 
                            outlieralgorithm='uncertain', p_bound=0.7 )
