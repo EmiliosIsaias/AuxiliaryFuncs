e899433ba8dde78e844c46a29e517e083fe54c34
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2024-06-27 19:09:36 +0200

    Runtime changes; bookmark

--- AnalyseOnlyBehaviour.m
+++ AnalyseOnlyBehaviour.m
@@ -1,7 +1,7 @@
 %% Analysing behaviour alone
 
 exp_path = ...
-    fullfile( "Z:\Emilio\SuperiorColliculusExperiments\Roller\Batch2_ephys\MC\GAD18\211229_F+C" );
+    fullfile( "Z:\Emilio\SuperiorColliculusExperiments\Roller\Batch8_ephys\eRNs\GADe54\220824_C_DV1900" );
 % Figure overwrite flag
 fowFlag = true;
 
@@ -44,7 +44,7 @@ end
 
 %% Run independently
 % User input!!
-consCond = [3:5];
+consCond = 3:4;
 Nccond = length( consCond );
 prmSubs = nchoosek(1:Nccond,2);
 
