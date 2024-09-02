bf98a9e72da0135c52bb5d5a6bdfc706be0f2b7e
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-09-02 17:15:21 +0200

    Finished the function

--- getClusterWaveforms64Channels.m
+++ getClusterWaveforms64Channels.m
@@ -1,12 +1,18 @@
-function waveforms = getClusterWaveforms64Channels( bin_path, spike_subs)
+function cwf = getClusterWaveforms64Channels( bin_path, spike_subs)
 
-
-cwf = zeros( 64, spikeWaveTime, numel( spkSubs ) );
-frewind( fID );
-for cspk = 1:numel(spkDists)
-fseek( fID, 2*64*(spkDists(cspk)-spikeSamples), "cof" );
-cwf(:,:,cspk) = fread( fID, [64, spikeWaveTime], "int16=>single" );
-fseek( fID, -2*64*(spikeSamples+1), 'cof' );
+cwf = [];
+spikeWaveTime = 75;
+spikeSamples = (spikeWaveTime - 1)/2;
+tocol = @(x) x(:);
+spkDists = [spike_subs(1); tocol( diff( spike_subs ) )];
+fID = fopen( bin_path, "r" );
+if fID >= 3
+    cwf = zeros( 64, spikeWaveTime, numel( spike_subs ) );
+    parfor cspk = 1:numel(spkDists)
+        fseek( fID, 128*(spkDists(cspk)-spikeSamples), "cof" );
+        cwf(:,:,cspk) = fread( fID, [64, spikeWaveTime], "int16=>single" );
+        fseek( fID, -128*(spikeSamples+1), 'cof' );
+    end
+    fclose(fID);
 end
-
 end
