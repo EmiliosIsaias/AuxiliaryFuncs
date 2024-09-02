a0b94b5e0c73e8af03facd32b15aeeed04481da1
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-09-02 18:24:07 +0200

    Removed the parallel as it introduced bugs

--- getClusterWaveforms64Channels.m
+++ getClusterWaveforms64Channels.m
@@ -8,7 +8,7 @@ spkDists = [spike_subs(1); tocol( diff( spike_subs ) )];
 fID = fopen( bin_path, "r" );
 if fID >= 3
     cwf = zeros( 64, spikeWaveTime, numel( spike_subs ) );
-    parfor cspk = 1:numel(spkDists)
+    for cspk = 1:numel(spkDists)
         fseek( fID, 128*(spkDists(cspk)-spikeSamples), "cof" );
         cwf(:,:,cspk) = fread( fID, [64, spikeWaveTime], "int16=>single" );
         fseek( fID, -128*(spikeSamples+1), 'cof' );
