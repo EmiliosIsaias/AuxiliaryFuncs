6c7ecdbfbb5eb01255a3d5e5e17600df7f07adec
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2026-03-10 13:33:30 +0100

    Added tick directionality

--- cleanAxis.m
+++ cleanAxis.m
@@ -1,6 +1,6 @@
 function [axOpts] = cleanAxis(ax)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
-axOpts = {'Box','off','Color','none'};
+axOpts = {'Box','off','Color','none','TickDir','out'};
 set( ax, axOpts{:} )
 end
